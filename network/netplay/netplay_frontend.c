97ccacf775991af38358d7f94843b6f3fa6e07c0
BloodotakuZ <scentia14@proton.me>
2025-11-21 02:52:43 +0000

    Defer GekkoNet host init until content is loaded

--- network/netplay/netplay_frontend.c
+++ network/netplay/netplay_frontend.c
@@ -12,6 +12,7 @@
 #include "netplay_defines.h"
 
 #include "../../configuration.h"
+#include "../../tasks/task_content.h"
 #include "../../retroarch.h"
 #include "../../verbosity.h"
 #include "../../performance_counters.h"
@@ -379,9 +380,12 @@ bool init_netplay(const char *server, unsigned port, const char *mitm_session)
    if (g_gekkonet.running)
       return true;
 
+   if (!gekkonet_init_session(server == NULL || string_is_empty(server), server, port))
+      return false;
+
    networking_driver_st.data = (netplay_t*)&g_gekkonet;
 
-   return gekkonet_init_session(server == NULL || string_is_empty(server), server, port);
+   return true;
 }
 
 void deinit_netplay(void)
@@ -429,7 +433,13 @@ bool netplay_driver_ctl(enum rarch_netplay_ctl_state state, void *data)
       {
          settings_t *settings = config_get_ptr();
          g_host_start_requested = true;
-         ret = init_netplay(NULL, settings->uints.netplay_port, NULL);
+
+         /* If there is no active core/content, defer session creation until the
+          * next content load triggers CMD_EVENT_NETPLAY_INIT. */
+         if (!content_is_inited())
+            ret = true;
+         else
+            ret = init_netplay(NULL, settings->uints.netplay_port, NULL);
          break;
       }
       case RARCH_NETPLAY_CTL_ENABLE_CLIENT:
